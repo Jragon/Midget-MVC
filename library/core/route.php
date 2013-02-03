1e1daf01e9be373c8e737eda552358e113336e72
Rory Anderson <rory@jragon.co.uk>
2013-02-03 22:00:05 +0000

    seperated out the functions

--- library/core/route.php
+++ library/core/route.php
@@ -1,39 +1,60 @@
 <?php
 
 class Route {
+	// sets the config variable, which will be passed to the controller
+	function __construct(&$config){
+		$this->config = $config;
+	}
+
 	public function load(){
-		global $config;
-		if(isset($_SERVER['PATH_INFO'])){
-			$route = explode('/', substr($_SERVER['PATH_INFO'], 1));
-			$controller = ucfirst(strtolower($route[0]));
-			$method = isset($route[1]) ? strtolower($route[1]) : 'index';
+		if($request = $this->requestInfo()){
+			if($this->checkPath($request['path'])){
+				$controller = new $request['controller']($this->config);
 
-			$path = APPPATH . "controllers/" . $route[0] . ".php";
-			if(realpath($path)){
-				require $path;
-				$controller = new $controller;
-
-				if($method != 'index'){
-					$variables = array();
-					$i = 0;
-					foreach($route as $vars){
-						if ($i >= 2)
-							$variables[] = $route[$i];
-						$i++;
-					}
-
-					call_user_func_array(array($controller, $method), $variables);
-				}else{
+				if($request['method'] != 'index')
+					call_user_func_array(array($controller, $request['method']), $request['arguments']);
+				else
 					$controller->index();
-				}
+			}else{
+				die;
 			}
 		}else{
-			$controller = $config['deafult_controller'];
+			$controller = $this->config['deafult_controller'];
 			require APPPATH . "controllers/" . $controller . ".php";
 			$controller = ucfirst($controller);
-			$controller = new $controller;
+			$controller = new $controller($this->config);
 			$method = 'index';
 			$controller->$method();
 		}
+	}
+
+	private function requestInfo(){
+		if(isset($_SERVER['PATH_INFO'])){
+			$route = explode('/', substr($_SERVER['PATH_INFO'], 1));
+
+			if(!empty($route[1])){
+				$request = array();
+				$request['controller'] = ucfirst(strtolower($route[0]));
+				$request['method'] = !empty($route[1]) ? strtolower($route[1]) : 'index';
+				$request['path'] = APPPATH . "controllers/" . $route[0] . ".php";
+
+				$i = 0;
+				foreach($route as $vars){
+					if ($i >= 2)
+						$route['arguments'][] = $route[$i];
+					$i++;
+				}				
+
+				return $request;
+			}else
+				return false;
+		}else
+			return false;
+	}
+
+	private function checkPath($path){
+		// not sure how to check this
+
+		return true;
 	}
 }
