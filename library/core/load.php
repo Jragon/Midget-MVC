9e38c37904e59b3429c1f04aad4363556875e7bf
Rory Anderson <rory@jragon.co.uk>
2013-02-04 18:18:26 +0000

    added user folder for user libraries, and moved path checks out side

--- library/core/load.php
+++ library/core/load.php
@@ -2,24 +2,39 @@
 
 class Load{
 
+	function __construct($config){
+		$this->config = $config;
+	}
+
 	// loads a specified model
 	public function model($model){
-		$model = strtolower($model);
-		if(file_exists(APPPATH . "models/$model.php")){
-			require(APPPATH . "models/$model.php");
+		$path = APPPATH . "models" . strtolower($model) . ".php"
+
+		if($this->checkPath($path)){
+			require($path);
 			$modelclass = ucfirst($model . '_Model');
+
 			// call function to include it in the controller
-			return new $modelclass;
+			return new $modelclass($this->config);
 		}
 	}
 
 	public function library($library){
-		$library = strtolower($library);
-		if(file_exists(LIBRARY . "$library.php")){
-			require(LIBRARY . "$library.php");
-			$libraryclass = ucfirst($library);
+		$path = LIBRARY . "user/" . strtolower($library) . ".php"
 
-			return new $libraryclass;
+		if($this->checkPath($path)){
+			require($path);
+			$libraryclass = ucfirst($library . '_library');
+
+			// call function to include it in the controller
+			return new $libraryclass($this->config);
 		}
+	}
+
+	private function checkPath($path){
+		if(file_exists($path) && preg_match("/" , APPPATH . "/i", realpath($path)))
+			return true;
+
+		return false;
 	}
 }
